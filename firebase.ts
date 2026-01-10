495a4f70777be4a4f367bf56d2770646774e5ecd
Keith Patel <keith@example.com>
2026-01-10 09:13:28 -0500

    Update Firebase configuration

--- firebase.ts
+++ firebase.ts
@@ -9,13 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // For now, we keep the hardcoded values to ensure it works immediately for you.
 // When deploying to Vercel, you should add these as Environment Variables.
 const firebaseConfig = {
-  apiKey: "AIzaSyAdNHwir2EWLMm4CuH-gVwkVWlPk-33tLU",
-  authDomain: "hapto2.firebaseapp.com",
-  databaseURL: "https://hapto2-default-rtdb.firebaseio.com",
-  projectId: "hapto2",
-  storageBucket: "hapto2.firebasestorage.app",
-  messagingSenderId: "790915133226",
-  appId: "1:790915133226:web:e6a49611817e23ce51b853"
+  apiKey: "AIzaSyDB0VXMo8LeaRZg-EXE-oGuQfvoYbuKeT4",
+  authDomain: "mandal-fdafb.firebaseapp.com",
+  projectId: "mandal-fdafb",
+  storageBucket: "mandal-fdafb.firebasestorage.app",
+  messagingSenderId: "387171044862",
+  appId: "1:387171044862:web:db374f201f0f8bc9ff44af"
 };
 
 const app = initializeApp(firebaseConfig);
